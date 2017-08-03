b7f72f4e8e8ed927c5a24117ecbb9993d8e1d716
Matt Hinchliffe <matt@maketea.co.uk>
2017-08-03 08:23:26 +0100

    Parser spec => tokenizer spec  🐿 v2.5.14

--- test/spec/tokenizer.spec.js
+++ test/spec/tokenizer.spec.js
@@ -1,13 +1,13 @@
 const fixtureSimple = require('../fixture/simple.json');
 const { expect } = require('chai');
-const subject = require('../../');
+const subject = require('../../lib/tokenizer');
 
-describe('lib/parser', () => {
+describe('lib/tokenizer', () => {
 	context('given simple queries', () => {
-		fixtureSimple.forEach(({ desc, input, output }) => {
+		fixtureSimple.forEach(({ desc, input, tokens }) => {
 			it(desc, () => {
 				const result = subject(input);
-				expect(result).to.deep.equal(output);
+				expect(result).to.deep.equal(tokens);
 			});
 		});
 	});
