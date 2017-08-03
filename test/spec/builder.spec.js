56f7613213c2da1e13ae1268032cf5f63e2161c5
Matt Hinchliffe <matt@maketea.co.uk>
2017-08-03 11:12:13 +0100

    Don't allow operators to be overloaded  🐿 v2.5.14

--- test/spec/builder.spec.js
+++ test/spec/builder.spec.js
@@ -1,10 +1,21 @@
-const fixtureSimple = require('../fixture/simple.json');
 const { expect } = require('chai');
 const subject = require('../../lib/builder');
+
+const fixtureSimple = require('../fixture/simple.json');
+const fixtureInvalid = require('../fixture/invalid.json');
 
 describe('lib/builder', () => {
 	context('given simple queries', () => {
 		fixtureSimple.forEach(({ desc, tokens, tree }) => {
+			it(desc, () => {
+				const result = subject(tokens);
+				expect(result).to.deep.equal(tree);
+			});
+		});
+	});
+
+	context('given invalid queries', () => {
+		fixtureInvalid.forEach(({ desc, tokens, tree }) => {
 			it(desc, () => {
 				const result = subject(tokens);
 				expect(result).to.deep.equal(tree);
--- test/spec/tokenizer.spec.js
+++ test/spec/tokenizer.spec.js
@@ -1,12 +1,23 @@
-const fixtureSimple = require('../fixture/simple.json');
 const { expect } = require('chai');
 const subject = require('../../lib/tokenizer');
 
+const fixtureSimple = require('../fixture/simple.json');
+const fixtureInvalid = require('../fixture/invalid.json');
+
 describe('lib/tokenizer', () => {
 	context('given simple queries', () => {
-		fixtureSimple.forEach(({ desc, input, tokens }) => {
+		fixtureSimple.forEach(({ desc, query, tokens }) => {
 			it(desc, () => {
-				const result = subject(input);
+				const result = subject(query);
+				expect(result).to.deep.equal(tokens);
+			});
+		});
+	});
+
+	context('given invalid queries', () => {
+		fixtureInvalid.forEach(({ desc, query, tokens }) => {
+			it(desc, () => {
+				const result = subject(query);
 				expect(result).to.deep.equal(tokens);
 			});
 		});
